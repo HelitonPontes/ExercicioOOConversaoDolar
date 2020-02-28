a2f7befb681b814fdd305734b5ccf92fdedb4e32
Heliton Pontes <36651605+HelitonPontes@users.noreply.github.com>
2020-02-28 18:39:00 -0300

    Initial commit

---
+++ ExercicioOOConversaoDolar/Dolar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioOOConversaoDolar
+{
+    class Dolar
+    {
+        public string Nome { get; set; }
+        public double Cotação { get; set; }
+        public double DolarCompra { get; set; }
+        public double Imposto = 6;
+        public double ValorReal { get; set; }
+
+        public Dolar()
+        {
+
+        }
+
+        public Dolar(string nome, double cotação, double dolarCompra)
+        {
+            Nome = nome;
+            Cotação = cotação;
+            DolarCompra = dolarCompra;
+        }
+
+        public Dolar(string nome, double cotação, double dolarCompra, double valorReal) : this(nome, 
+            cotação, dolarCompra)
+        {
+            ValorReal = valorReal;
+        }
+
+        public Dolar(string nome, double cotação, double dolarCompra, double imposto, 
+            double valorReal) : this(nome, cotação, dolarCompra, imposto)
+        {
+            ValorReal = valorReal;
+        }
+
+        public double Resultado()
+        {
+            ValorReal = (DolarCompra*Cotação) /100*Imposto + (DolarCompra * Cotação);
+           
+
+            return (ValorReal);
+        }
+
+
+
+    }
+}
---
+++ ExercicioOOConversaoDolar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioOOConversaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("XXXXXXX Programa Dolar XXXXXXXXX");
+            Console.WriteLine("----------------------------------");
+
+            Dolar Dr = new Dolar();
+    
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Dolar");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------");
+
+            Console.Write("Cadastre Nome: ");
+            Dr.Nome = Console.ReadLine();
+            Console.Write("Cotação: ");
+            Dr.Cotação = double.Parse(Console.ReadLine());
+            Console.Write("Valor em Dolar: ");
+            Dr.DolarCompra= double.Parse(Console.ReadLine());
+          
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------");
+
+            Console.WriteLine("Nome Ciente: "+Dr.Nome+" Cotação: "+Dr.Cotação+
+                " Valor de Dolar para Compra: "+ Dr.DolarCompra);
+
+
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------");
+
+            Console.Write(Dr.Resultado());
+            Console.WriteLine("");
+            Console.WriteLine(Dr.Resultado());
+
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
